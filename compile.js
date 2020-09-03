9be65083d744f978747e93db8f68e8816f405b22
B1ao <70520921+B1ao@users.noreply.github.com>
2020-09-03 16:09:05 +0700

    first commit

---
+++ compile.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const helloPath = path.resolve(__dirname, 'contracts', 'hello.sol');
+const source = fs.readFileSync(helloPath, 'utf8');
+const compiledSource = solc.compile(source, 1).contracts[':Hello'];
+
+module.exports = compiledSource;
+
+console.log(compiledSource);
